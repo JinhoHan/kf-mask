13d09530773fc9298376ddcc2a9b34d3917b6ec3
jinhoHan <bedmil85@gmail.com>
2020-05-08 17:12:17 +0900

    Updates

---
+++ precache-manifest.20ffada3e290d2e6667404fdba9a11af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb7448f49ba15e1c2a3f65740a45ce20",
+    "url": "/kf-mask/index.html"
+  },
+  {
+    "revision": "ee3641c3a321ee842c00",
+    "url": "/kf-mask/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "75d008f8ee53b95d0841",
+    "url": "/kf-mask/static/css/main.9da48102.chunk.css"
+  },
+  {
+    "revision": "ee3641c3a321ee842c00",
+    "url": "/kf-mask/static/js/2.6be5dfb5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/kf-mask/static/js/2.6be5dfb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75d008f8ee53b95d0841",
+    "url": "/kf-mask/static/js/main.db20fc7d.chunk.js"
+  },
+  {
+    "revision": "8dc79c1358a71da4ef03",
+    "url": "/kf-mask/static/js/runtime-main.cb4f796a.js"
+  }
+]);
